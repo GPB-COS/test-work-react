33ec4fe036415100a9a95a5beaa03f5bb907a47e
PAV <popov.pav.aleksei@gmail.com>
2021-05-06 19:59:40 +0300

    delete commentaries

--- test 2/client/src/App.js
+++ test 2/client/src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Header } from './components/Header/Header';
 import { ItemList } from './components/ItemList/ItemList';
-import { useState } from 'react';
-import { Loader } from './components/Loader/Loader';
 import { ItemDetails } from './components/ItemDetails/ItemDetails';
 import { Home } from './components/Home/Home';
 
 function App() {
 
-  // const [loader, setLoader] = useState(true)
   return (
     <Router>
       <Header />
@@ -20,7 +17,6 @@ function App() {
         <Route exact path="/:id/details">
           <ItemDetails />
         </Route>
-        {/* <Loader /> */}
         <Route exact path="/">
           <Home />
         </Route>
